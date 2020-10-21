f5f45e99e5ce341200be2f0f0cb35411531e0596
Minha Kwon <minha61@naver.com>
2020-10-21 23:15:34 +0900

    Update Permutation.java

--- Permutaion_Combination/src/Permutation.java
+++ Permutaion_Combination/src/Permutation.java
@@ -8,21 +8,39 @@
 public class Permutation {
 	public static void main(String[] args) {
 		int[] arr = {1, 2, 3}; //순열을 만들 배열 
-		int n = arr.length;
+		int n = arr.length; //배열의 길이 
 		int[] output = new int[n]; //순열 출력을 위한 배열 
-		boolean[] visited = new boolean[n]; // 중복해서 뽑지 않기 위해, 배열의 원소를 방문했는지를 체크하기 위한 배열 
-       
-        System.out.println("-------- 1. DFS ---------");
-		per1(arr, output, visited, 0, n, 3); //r = 3, 3개를 뽑을 것 
-		
-  
-        System.out.println("\n-------- 2. Swap ---------");
-		System.out.println();
-		per2(arr, 0, n, 3);
+		boolean[] visited = new boolean[n]; //중복해서 뽑지 않기 위해 방문했는지를 체크하는 배열 
+
+		System.out.println("-------- 1. Swap ---------");
+		per1(arr, 0, n, 3);
+
+		System.out.println("\n-------- 2. DFS ---------");
+		per2(arr, output, visited, 0, n, 3); //r = 3, 3개를 뽑을 것 
 	}
 
-	//1. DFS를 이용해 구현  - 순서를 지키면서 n 개중에서 r 개를 뽑는 경우
-	static void per1(int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
+	//1. Swap 함수를 이용해 구현 - 순서 없이 n 개중에서 r 개를 뽑는 경우
+	static void per1(int[] arr, int depth, int n, int r) {
+		if(depth == r) {
+			print(arr, r);
+			return;
+		}
+
+		for(int i=depth; i<n; i++) {
+			swap(arr, depth, i);
+			per1(arr, depth + 1, n, r);
+			swap(arr, depth, i);
+		}
+	}
+
+	static void swap(int[] arr, int depth, int i) { //두 배열의 값을 바꾸는 Swap 함수 
+		int temp = arr[depth];
+		arr[depth] = arr[i];
+		arr[i] = temp;
+	}
+
+	//2. DFS를 이용해 구현  - 순서를 지키면서 n 개중에서 r 개를 뽑는 경우
+	static void per2(int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
 		if(depth == r) {
 			print(output, r); //순열 출력을 위한 print 함수 
 			return;
@@ -32,30 +50,10 @@ public class Permutation {
 			if(visited[i] != true) {
 				visited[i] = true;
 				output[depth] = arr[i];
-				per1(arr, output, visited, depth + 1, n, r);    
+				per2(arr, output, visited, depth + 1, n, r);    
 				visited[i] = false;
 			}
 		}
-	}
-
-	//2. Swap 함수를 이용해 구현 - 순서 없이 n 개중에서 r 개를 뽑는 경우
-	static void per2(int[] arr, int depth, int n, int r) {
-		if(depth == r) {
-			print(arr, r);
-			return;
-		}
-
-		for(int i=depth; i<n; i++) {
-			swap(arr, depth, i);
-			per2(arr, depth + 1, n, r);
-			swap(arr, depth, i);
-		}
-	}
-
-	static void swap(int[] arr, int depth, int i) { //두 배열의 값을 바꾸는 Swap 함수 
-		int temp = arr[depth];
-		arr[depth] = arr[i];
-		arr[i] = temp;
 	}
 
 	// 배열 출력
