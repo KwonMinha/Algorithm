d936ad417a4ccbc6ef793a769f01d82047c0a49d
Minha Kwon <minha61@naver.com>
2020-11-17 22:15:38 +0900

    Update DFS_Array.java

--- DBFS_Array/src/DFS_Array.java
+++ DBFS_Array/src/DFS_Array.java
@@ -32,7 +32,7 @@ public class DFS_Array {
 		int m = sc.nextInt(); // 간선의 개수 
 		int v = sc.nextInt(); // 탐색을 시작할 정점의 번호 
 
-		boolean visited[] = new boolean[n + 1]; // 방문 순서를 위해 오름차순 정렬 
+		boolean visited[] = new boolean[n + 1]; // 방문 여부를 검사할 배열  
 
 		int[][] adjArray = new int[n+1][n+1];
 
