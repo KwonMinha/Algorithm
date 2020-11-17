215e12cdf11bc3aa1f1b132eb3b429dd9f8b38da
Minha Kwon <minha61@naver.com>
2020-11-18 04:36:00 +0900

    Create TreeOrderQueue.java

---
+++ Tree/src/TreeOrderQueue.java
@@ -0,0 +1,40 @@
+/**
+ * @author Gwon Minha
+ * 
+ * 큐를 이용해 이진 트리를 레벨 순회한 결과를 출력하는 프로그램 
+
+   입력 : 첫 번째 줄에 트리의 노드 개수 n이 주어진다. ( 1 ≤ n ≤ 100 ) 
+         두 번째 줄부터 트리의 정보가 주어진다. 각 줄은 3개의 숫자 a, b, c로 이루어지며, 
+         그 의미는 노드 a의 왼쪽 자식노드가 b, 오른쪽 자식노드가 c라는 뜻이다. 
+         자식노드가 존재하지 않을 경우에는 -1이 주어진다.
+
+   출력 : 레벨 순회를 한 결과를 출력한다.
+
+   예제 :  
+
+6
+0 1 2
+1 3 4
+2 -1 5
+3 -1 -1
+4 -1 -1
+5 -1 -1
+
+       0
+      / \
+     1   2
+    / \   \
+   3   4   5
+   
+ */
+
+import java.util.*;
+
+public class TreeOrderQueue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
