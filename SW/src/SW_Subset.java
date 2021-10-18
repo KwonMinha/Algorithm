d9b3c6e7ded3fb780c9c9f8c3b49b9021ec619e9
Minha Kwon <minha61@naver.com>
2021-10-18 17:37:31 +0900

    Update SW_Subset.java

--- SW/src/SW_Subset.java
+++ SW/src/SW_Subset.java
@@ -1,6 +1,7 @@
 
 public class SW_Subset {
-	static int[] arr = {-1, 3, -9, 6, 7, -6, 1, 5, 4, -2};
+	// static int[] arr = {-1, 3, -9, 6, 7, -6, 1, 5, 4, -2};
+	static int[] arr = {1, 2, 3};
 	static int[] output;
 
 	public static void main(String[] args) {
@@ -13,12 +14,16 @@ public class SW_Subset {
 	
 	public static void combination(int n, int r) {
 		if(r == 0) {
-			calculate();
+			// calculate();
+			print();
 		} else if(n < r) {
+			System.out.println("n : " + n + ", r : " + r + " -> return");
 			return;
 		} else {
 			output[r-1] = arr[n-1];
+			System.out.println("1 ---- r-1 : " + (r-1) + ", n-1 : " + (n-1) + "\n");
 			combination(n-1, r-1);
+			System.out.println("2 ---- n-1 : " + (n-1) + ", r : " + r + "\n");
 			combination(n-1, r);
 		}
 	}
@@ -38,7 +43,7 @@ public class SW_Subset {
 		for(int i = 0; i < output.length; i++) {
 			System.out.print(output[i] + " ");
 		}
-		System.out.println();
+		System.out.println("\n");
 	}
 
 }
